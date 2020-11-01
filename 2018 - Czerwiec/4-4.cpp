bb5ec16edb6b44a9153ac56b2be9a12e802d9a76
Stanisław Borodziuk <borodziukstanislaw@gmail.com>
2020-11-01 20:36:56 +0100

    Dodano zadanie 4.4

---
+++ 2018 - Czerwiec/4-4.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ofstream wyj("wynik4_4.txt");
+    ifstream wej1("dane1.txt");
+    ifstream wej2("dane2.txt");
+
+    for(int i=0; i<1000; i++)
+    {
+        int element_a = 0, element_b = 0, dane1[10], dane2[10], out[20];
+
+        for(int j=0; j<10; j++)
+        {
+            wej1 >> dane1[j];
+        }
+        for(int j=0; j<10; j++)
+        {
+            wej2 >> dane2[j];
+        }
+
+        while(element_a<=9 && element_b<=9){
+            if(dane1[element_a] <= dane2[element_b]){
+                out[element_a + element_b] = dane1[element_a];
+                element_a++;
+            }else{
+                out[element_a + element_b] = dane2[element_b];
+                element_b++;
+            }
+        } 
+
+        int j = element_a+element_b;
+        if(element_a != 10){
+            cout << element_a << " " << element_b << endl;
+            while(element_a <= 9){
+                out[j] = dane1[element_a];
+                element_a++;
+                j++;
+            }
+        }
+        if(element_b != 10){
+            cout << element_a << " " << element_b << endl;
+            while(element_b <= 9){
+                out[j] = dane2[element_b];
+                element_b++;
+                j++;
+            }
+        }
+
+        for(int j=0; j<20; j++)
+            wyj << out[j] << " ";
+        wyj << endl;
+    }
+    
+    getchar();
+    getchar();
+    return 0;
+}
