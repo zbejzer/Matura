35945767d65bb995af1c5dc8256edb042c5338a1
Stanisław Borodziuk <borodziukstanislaw@gmail.com>
2022-03-23 19:20:15 +0100

    Zaktualizowano maturę 2020 Czerwiec
    Zmiana nazwy folderu na adekwatną
    Poprawa rozwiązania zadania 4

---
+++ 2020 - Czerwiec/Zadanie 4/Zadanie 4-2/Zadanie 4-2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <fstream>
+#include <array>
+#include <string>
+
+#define DATA_FILE "pary.txt" // przyklad pary 
+#define DATA_SIZE 100
+
+using namespace std;
+
+string longestConsistent(string word) {
+    string longest_consist = "";
+    int p = 0, q = 0;
+
+    while (q < word.size()) {
+        if (word.at(p) != word.at(q)) {
+            p = q;
+        }
+        else {
+            if (word.substr(p, q - p + 1).size() > longest_consist.size()) {
+                longest_consist = word.substr(p, q - p + 1);
+            }
+        }
+        q++;
+    }
+
+    return longest_consist;
+}
+
+int main()
+{
+    ifstream data(DATA_FILE);
+
+    for (size_t i = 0; i < DATA_SIZE; i++)
+    {
+        int number;
+        string text;
+
+        data >> number >> text;
+
+        cout << longestConsistent(text) << " " << longestConsistent(text).size() << endl;
+    }
+
+    return 0;
+}
---
+++ 2020 - Czerwiec/Zadanie 4/Zadanie 4-3/Zadanie 4-3.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <fstream>
+#include <array>
+#include <string>
+#include <cmath>
+
+#define DATA_FILE "pary.txt" // przyklad pary 
+#define DATA_SIZE 100
+
+using namespace std;
+
+bool isSmaller(string a, string b) {
+    for (size_t i = 0; i < min(a.size(), b.size()); i++)
+    {
+        if (a.at(i) < b.at(i)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int main()
+{
+    ifstream data(DATA_FILE);
+    string smallest_word = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+
+    for (size_t i = 0; i < DATA_SIZE; i++)
+    {
+        int number;
+        string text;
+
+        data >> number >> text;
+
+        if (number == text.size()) {
+            if (isSmaller(text, smallest_word) || text.size() < smallest_word.size()) {
+                smallest_word = text;
+            }
+        }
+    }
+
+    cout << smallest_word.size() << " " << smallest_word << endl;
+
+    return 0;
+}
